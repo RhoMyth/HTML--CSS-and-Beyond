eef3818ed7ed1f0b24c36afb180d6e34d62bd8b4
Romit Raj <romitrajrare@gmail.com>
2026-01-08 02:24:16 +0530

    Add variable type checks and object manipulation examples
    Added console logs to display types of variables, introduced new variables with different types, and demonstrated object creation and property addition for learning purposes.

--- variables.js
+++ variables.js
@@ -5,3 +5,27 @@ var a= 10;
 var b= 7;
 var sum = a+b;
 console.log("The sum of a and b is: " + sum);
+console.log(typeof a, typeof b, typeof sum);
+const name = "RhoMyth  ";
+console.log("My name is " + name);
+
+
+let x = 22;
+let y = "Romy Bhai";
+let z = undefined;
+let p = true;
+let q = null;
+
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q);
+
+let o = {
+    name: "Romy",
+    "current": "Learning JavaScript",
+
+
+}
+console.log(o);
+
+console.log(o);
+o.salary= "unemployed";
+console.log(o);
