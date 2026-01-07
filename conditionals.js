a501a93cbfb30b1a80ed92c124a605eed4633920
Romit Raj <romitrajrare@gmail.com>
2026-01-08 03:04:40 +0530

    Add JS conditionals demo and HTML scaffold
    Introduces conditionals.html with a basic HTML structure and conditionals.js demonstrating JavaScript arithmetic and conditional statements. Also adds a tempCodeRunnerFile.js for testing variable assignment.

---
+++ conditionals.js
@@ -0,0 +1,17 @@
+console.log("Welcome to JavaScript Programming!");
+
+
+let age=22;
+let grace=7;
+
+console.log(age + grace);
+console.log(age - grace);
+console.log(age / grace);
+console.log(age % grace);
+console.log(age ** grace);
+if(age==18){
+    console.log("You can Vote");
+}
+else{
+    console.log("You cannot Vote");
+}
