0bf74d5341cddb953b864589671b73e47599a9de
Romit Raj <romitrajrare@gmail.com>
2026-01-08 03:19:52 +0530

    Update conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -1,7 +1,7 @@
 console.log("Welcome to JavaScript Programming!");
 
 
-let age=22;
+let age=5;
 let grace=7;
 
 console.log(age + grace);
@@ -12,6 +12,19 @@ console.log(age ** grace);
 if(age==18){
     console.log("You can Vote");
 }
+else if(age == 5){
+    console.log("Too Small to Judge");
+
+}
+else if (age == 0){
+    console.log("You're not born yet??");
+}
 else{
     console.log("You cannot Vote");
 }
+
+
+let a = 10;
+let b = 20;
+let c = a>b ? (a-b) : (b-a);
+console.log(c);
